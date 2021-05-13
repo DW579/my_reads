0192e6925b8c2ff0dcba7184bc49f34694b25785
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2021-05-13 07:13:28 -0700

    Created Main.js, Search.js, and ViewAll.js in pages folder and implemented them into Route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Search from "./pages/Search";
+import ViewAll from "./pages/ViewAll";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Route exact path="/" render={() => <Main></Main>}></Route>
+                <Route path="/search" render={() => <Search></Search>}></Route>
+                <Route path="/view_all" render={() => <ViewAll></ViewAll>}></Route>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/pages/ViewAll.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ViewAll extends Component {
+
+    render() {
+        return (
+            <div>View All page</div>
+        )
+    }
+}
+
+export default ViewAll;
