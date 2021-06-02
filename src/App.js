57b1d4fe4936f674d803af675a8b2a5f99a5e0ce
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2021-06-02 08:03:44 -0700

    Restarting code to start

--- src/App.js
+++ src/App.js
@@ -3,63 +3,9 @@ import { Route } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import Main from "./pages/Main";
 import Search from "./pages/Search";
-import ViewAll from "./pages/ViewAll";
-import * as BooksAPI from "./utils/BooksAPI";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
-    state = {
-        currentlyReading: [],
-        wantToRead: [],
-        read: [],
-    };
-
-    componentDidMount() {
-        BooksAPI.getAll().then((books) => {
-            this.setState(() => ({
-                currentlyReading: books.filter(
-                    (book) => book.shelf === "currentlyReading"
-                ),
-                wantToRead: books.filter((book) => book.shelf === "wantToRead"),
-                read: books.filter((book) => book.shelf === "read"),
-            }));
-        });
-    }
-
-    updateShelf = (bookId, currentShelf, moveToShelf) => {
-        const book = this.state[currentShelf].filter(
-            (book) => book.id === bookId
-        );
-
-        this.setState(() => ({
-            currentlyReading:
-                moveToShelf === "currentlyReading"
-                    ? this.state.currentlyReading.concat(book[0])
-                    : this.state.currentlyReading.filter(
-                          (book) => book.id !== bookId
-                      ),
-            wantToRead:
-                moveToShelf === "wantToRead"
-                    ? this.state.wantToRead.concat(book[0])
-                    : this.state.wantToRead.filter(
-                          (book) => book.id !== bookId
-                      ),
-            read:
-                moveToShelf === "read"
-                    ? this.state.read.concat(book[0])
-                    : this.state.read.filter((book) => book.id !== bookId),
-        }));
-
-        BooksAPI.update(bookId, moveToShelf);
-    };
-
-    addToShelf = (book, moveToShelf) => {
-        this.setState(() => ({
-            currentlyReading: ((moveToShelf === "currentlyReading") ? this.state.currentlyReading.concat(book) : this.state.currentlyReading),
-            wantToRead: ((moveToShelf === "wantToRead") ? this.state.wantToRead.concat(book) : this.state.wantToRead),
-            read: ((moveToShelf === "read") ? this.state.read.concat(book) : this.state.read)
-        }))
-    }
 
     render() {
         return (
@@ -71,27 +17,14 @@ class App extends Component {
                                 exact
                                 path="/"
                                 render={() => (
-                                    <Main
-                                        currentlyReading={
-                                            this.state.currentlyReading
-                                        }
-                                        wantToRead={this.state.wantToRead}
-                                        read={this.state.read}
-                                        updateShelf={this.updateShelf}
-                                    ></Main>
+                                    <Main></Main>
                                 )}
                             ></Route>
                             <Route
                                 path="/search"
                                 render={() => (
-                                    <Search
-                                        addToShelf={this.addToShelf}
-                                    ></Search>
+                                    <Search></Search>
                                 )}
-                            ></Route>
-                            <Route
-                                path="/view_all"
-                                render={() => <ViewAll></ViewAll>}
                             ></Route>
                         </Col>
                     </Row>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,119 +1,13 @@
 import React, { Component } from "react";
-import {
-    Row,
-    Col,
-    InputGroup,
-    FormControl,
-    Card,
-    DropdownButton,
-    Dropdown,
-} from "react-bootstrap";
 import * as BooksAPI from "../utils/BooksAPI";
-import { Link } from "react-router-dom";
 
 class Search extends Component {
-    state = {
-        books: [],
-    };
-
-    handleSearch = (event) => {
-        if (event.code === "Enter") {
-            BooksAPI.search(event.target.value).then((books) => {
-                if (books.error !== "empty query") {
-                    this.setState(() => ({
-                        books,
-                    }));
-                }
-            });
-
-            event.target.value = "";
-        }
-    };
-
-    updateSearchState = () => {
-        console.log("update search");
-    };
 
     render() {
-        const { addToShelf } = this.props;
 
         return (
             <div>
-                <Row>
-                    <Col>
-                        <InputGroup size="lg" className="mb-3">
-                            <FormControl
-                                aria-label="Large"
-                                aria-describedby="inputGroup-sizing-sm"
-                                placeholder="Search Titles or Authors"
-                                onKeyPress={(event) => this.handleSearch(event)}
-                            />
-                        </InputGroup>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Link to="/">
-                            Back
-                        </Link>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        {this.state.books.map((book) => (
-                            <Card
-                                style={{
-                                    width: "18rem",
-                                    display: "inline-block",
-                                }}
-                                key={book.id}
-                            >
-                                <Card.Img
-                                    style={{ width: "128px" }}
-                                    variant="top"
-                                    src={book.imageLinks.thumbnail}
-                                />
-                                <Card.Body>
-                                    <Card.Title>{book.title}</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the
-                                        card title and make up the bulk of the
-                                        card's content.
-                                    </Card.Text>
-                                    <DropdownButton
-                                        id="dropdown-basic-button"
-                                        title="Move to..."
-                                    >
-                                        <Dropdown.Item
-                                            onClick={() =>
-                                                addToShelf(
-                                                    book,
-                                                    "currentlyReading"
-                                                )
-                                            }
-                                        >
-                                            Currently Reading
-                                        </Dropdown.Item>
-                                        <Dropdown.Item
-                                            onClick={() =>
-                                                addToShelf(book, "wantToRead")
-                                            }
-                                        >
-                                            Want to read
-                                        </Dropdown.Item>
-                                        <Dropdown.Item
-                                            onClick={() =>
-                                                addToShelf(book, "read")
-                                            }
-                                        >
-                                            Read
-                                        </Dropdown.Item>
-                                    </DropdownButton>
-                                </Card.Body>
-                            </Card>
-                        ))}
-                    </Col>
-                </Row>
+                <h1>Search Page</h1>
             </div>
         );
     }
